ac2662122a10896f2dd0c59a8eccbcda1384f465
Mexahoid <mirramax97@Mail.ru>
2018-03-07 14:52:43 +0300

    Ыфы

--- app/src/main/java/mex/quiz/QuizActivity.java
+++ app/src/main/java/mex/quiz/QuizActivity.java
@@ -2,6 +2,7 @@ package mex.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,10 +13,58 @@ public class QuizActivity extends AppCompatActivity {
     private Button trueButton, falseButton, quitButton;
     private int counter = 0;
     private boolean locker = false;
+    public static final String SAS = "QuizActivity";
+    public static final String MARKER = "SAS";
+    public static final String LOCKER_MARKER = "SOS";
+
+    @Override
+    protected void onStart(){
+        Log.d(SAS, "On start");
+        super.onStart();
+    }
+
+
+    @Override
+    protected void onStop(){
+        Log.d(SAS, "On stop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume(){
+        Log.d(SAS, "On resume");
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause(){
+        Log.d(SAS, "On pause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy(){
+        Log.d(SAS, "On destroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        outState.putInt(MARKER, counter);
+        outState.putInt(LOCKER_MARKER, locker ? 0 : 1);
+        super.onSaveInstanceState(outState);
+    }
+
     @Override
     // Сделать несколько вопросов
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);   // Всегда самой первой
+        if(savedInstanceState != null && savedInstanceState.containsKey(MARKER))
+        {
+            counter = (int)savedInstanceState.get(MARKER) - 1;
+            locker = (int)savedInstanceState.get(LOCKER_MARKER) == 0;
+        }
         setContentView(R.layout.activity_quiz);  // id xml внутри системы   заполняет экран макетом
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
