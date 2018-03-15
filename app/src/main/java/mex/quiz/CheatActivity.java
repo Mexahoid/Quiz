8e21ef4c5f68f648e6295d86c5a2cd1f7d3b0e5d
Mexahoid <mirramax97@Mail.ru>
2018-03-15 22:30:59 +0300

    Сделал новую версию

--- app/src/main/java/mex/quiz/CheatActivity.java
+++ app/src/main/java/mex/quiz/CheatActivity.java
@@ -6,37 +6,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
 
-    private static final String EXTRA_ANSWER_IS_TRUE = "Sos";
-     static boolean answerIsTrue;
-    private Button returnButton, cheatButton;
+    private static final String EXTRA_ANSWER = "mex.quiz.ait";
+    private String answer;
+    private Button cheatButton;
+    private TextView question, cheatyAnswer;
     private final String SAS = "Lol";
-    private boolean wasCheated;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        answerIsTrue = false;
 
-        returnButton = findViewById(R.id.return_button);
+        setContentView(R.layout.activity_cheat);
         cheatButton = findViewById(R.id.cheat_button);
+        question = findViewById(R.id.question_cheat_answer);
+        cheatyAnswer = findViewById(R.id.question_cheat_question);
 
         cheatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 setAnswerShowResult(true);
+
+                question.setText(String.format("%s %s", CheatActivity.this.getResources().getString(R.string.cheat_true), answer));
+                cheatButton.setEnabled(false);
+                cheatButton.setVisibility(View.INVISIBLE);
+
+                cheatyAnswer.setText(R.string.cheat_cheated);
             }
         });
-        setContentView(R.layout.activity_cheat);
+
+        answer = getIntent().getStringExtra(EXTRA_ANSWER);
     }
 
 
-    public static Intent newIntent(Context packageContext, boolean answerTrue){
-        //Intent i = new Intent(packageContext, CheatActivity.class);
-        Intent i = new Intent();
-        answerIsTrue = answerTrue;
-        i.putExtra(EXTRA_ANSWER_IS_TRUE, answerTrue);
+    public static Intent newIntent(Context packageContext, String answer){
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER, answer);
         return i;
     }
 
--- app/src/main/java/mex/quiz/QuizActivity.java
+++ app/src/main/java/mex/quiz/QuizActivity.java
@@ -10,22 +10,269 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.os.CountDownTimer;
 
+
 public class QuizActivity extends AppCompatActivity {
-    private Button trueButton, falseButton, quitButton, cheatButton;
+    private Button quitButton, cheatButton, startOverButton;
+
+    private Button[] buttons = new Button[4];
+    private int[] answerColors = new int[15];
+
+    private TextView question, questionInfo;
     private int counter = 0;
     private boolean locker = false;
+    private boolean cheated = false;
     public static final String SAS = "QuizActivity";
     public static final String MARKER = "SAS";
     public static final String LOCKER_MARKER = "SOS";
-    public static final String EXTRA_ANS = "Kek";
+    public static final String CHEATER_MARKER = "LOL";
+    public static final String COLORS_MARKER = "KEK";
     public static int REQUEST_CHEAT = 0;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        outState.putInt(MARKER, counter);
+        outState.putInt(LOCKER_MARKER, locker ? 0 : 1);
+        outState.putInt(CHEATER_MARKER, cheated ? 0 : 1);
+        outState.putIntArray(COLORS_MARKER, answerColors);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    // Сделать несколько вопросов
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);   // Всегда самой первой
+        setContentView(R.layout.activity_quiz);  // id xml внутри системы   заполняет экран макетом
+        if(savedInstanceState != null &&
+                savedInstanceState.containsKey(MARKER) &&
+                savedInstanceState.containsKey(LOCKER_MARKER) &&
+                savedInstanceState.containsKey(CHEATER_MARKER)&&
+                savedInstanceState.containsKey(COLORS_MARKER))
+        {
+            counter = (int)savedInstanceState.get(MARKER) - 1;
+            locker = (int)savedInstanceState.get(LOCKER_MARKER) == 0;
+            cheated = (int)savedInstanceState.get(CHEATER_MARKER) == 0;
+            answerColors = (int[])savedInstanceState.get(COLORS_MARKER);
+            colorRows();
+        }
+
+
+        question = findViewById(R.id.question_text);
+        questionInfo = findViewById(R.id.question_choices);
+
+        initButtons();
+        changeText();
+
+        cheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(CheatActivity.newIntent(QuizActivity.this, findAnswer()), REQUEST_CHEAT);
+            }
+        });
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               finish();
+            }
+        });
+
+        startOverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                for(counter = 1; counter < 16; counter++)
+                    colorRow(0, counter);
+                counter = 0;
+                switchChoiceButtons();
+                startOverButton.setEnabled(false);
+                startOverButton.setVisibility(View.INVISIBLE);
+                changeText();
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == REQUEST_CHEAT) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                cheated = true;
+                boolean sas = data.getBooleanExtra("Lol", false);
+            }
+        }
+    }
+
+    private void changeText(){
+        String naming = "question_" + ++counter;
+        int qid = this.getResources().getIdentifier(naming, "string", getPackageName());
+        naming = "question_" + counter + "_text";
+        int qiid = this.getResources().getIdentifier(naming, "string", getPackageName());;
+        if(qid == 0 || qiid == 0)
+        {
+            question.setText("Вопросы кончились. Действие?");
+            questionInfo.setText("");
+            switchChoiceButtons();
+            quitButton.setEnabled(true);
+            quitButton.setVisibility(View.VISIBLE);
+            startOverButton.setEnabled(true);
+            startOverButton.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            question.setText(qid);
+            questionInfo.setText(qiid);
+        }
+    }
+
+    private void switchChoiceButtons(){
+        for (int i = 0; i < 4; i++)
+        {
+            buttons[i].setEnabled(!buttons[i].isEnabled());
+            buttons[i].setVisibility(buttons[i].isEnabled() ? View.VISIBLE : View.INVISIBLE);
+        }
+    }
+
+    private String findAnswer(){
+        String naming = "question_" + counter + "_truth";
+        return getStringResourceValue(naming);
+    }
+
+    private void initButtons(){
+        buttons[0] = findViewById(R.id.button_1);
+        buttons[1] = findViewById(R.id.button_2);
+        buttons[2] = findViewById(R.id.button_3);
+        buttons[3] = findViewById(R.id.button_4);
+
+        buttons[0].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choiceButtonHandler(getStringResourceValue("question_button_" + 1));
+            }
+        });
+
+        buttons[1].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choiceButtonHandler(getStringResourceValue("question_button_" + 2));
+            }
+        });
+        buttons[2].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choiceButtonHandler(getStringResourceValue("question_button_" + 3));
+            }
+        });
+        buttons[3].setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                choiceButtonHandler(getStringResourceValue("question_button_" + 4));
+            }
+        });
+
+        startOverButton = findViewById(R.id.start_over_button);
+        quitButton = findViewById(R.id.quit_button);
+        cheatButton = findViewById(R.id.cheat_button_invoke);
+    }
+
+    private void choiceButtonHandler(String selection){
+        final int resId = cheated
+                ? R.string.cheated
+                : (findAnswer().equals(selection)
+                        ? R.string.true_ans
+                        : R.string.false_ans);
+        if(!locker)
+        {
+            locker = true;
+            Toast.makeText(QuizActivity.this, resId, Toast.LENGTH_SHORT).show();
+            new CountDownTimer(2000, 1000) {
+                public void onTick(long millisUntilFinished) {
+                }
+                public void onFinish() {
+                    colorRow(resId, counter);
+                    changeText();
+                    locker = false;
+                }
+            }.start();
+            cheated = false;
+        }
+    }
+
+    private String getStringResourceValue(String naming){
+        int id = this.getResources().getIdentifier(naming, "string", getPackageName());
+        if(id != 0){
+            return this.getResources().getString(id);
+        }
+        return null;
+    }
+
+    private TextView getRow(int pos){
+        switch (pos){
+            case 1:
+                return findViewById(R.id.row_1);
+            case 2:
+                return findViewById(R.id.row_2);
+            case 3:
+                return findViewById(R.id.row_3);
+            case 4:
+                return findViewById(R.id.row_4);
+            case 5:
+                return findViewById(R.id.row_5);
+            case 6:
+                return findViewById(R.id.row_6);
+            case 7:
+                return findViewById(R.id.row_7);
+            case 8:
+                return findViewById(R.id.row_8);
+            case 9:
+                return findViewById(R.id.row_9);
+            case 10:
+                return findViewById(R.id.row_10);
+            case 11:
+                return findViewById(R.id.row_11);
+            case 12:
+                return findViewById(R.id.row_12);
+            case 13:
+                return findViewById(R.id.row_13);
+            case 14:
+                return findViewById(R.id.row_14);
+            case 15:
+                return findViewById(R.id.row_15);
+        }
+        return null;
+    }
+
+    private void colorRow(int type, int rowPos){
+        TextView row = getRow(rowPos);
+
+        answerColors[rowPos - 1] = type;
+        switch (type){
+            case 0:
+                row.setBackgroundResource(R.color.question_not_answered);
+                answerColors[rowPos - 1] = 0;
+                break;
+            case R.string.true_ans:
+                row.setBackgroundResource(R.color.question_answered_true_back);
+                break;
+            case R.string.false_ans:
+                row.setBackgroundResource(R.color.question_answered_false_back);
+                break;
+            case R.string.cheated:
+                row.setBackgroundResource(R.color.question_cheated_back);
+                break;
+        }
+    }
+
+    private void colorRows(){
+        for (int i = 0; i < 15; i++){
+            colorRow(answerColors[i], i + 1);
+        }
+    }
 
     @Override
     protected void onStart(){
         Log.d(SAS, "On start");
         super.onStart();
     }
-
 
     @Override
     protected void onStop(){
@@ -39,7 +286,6 @@ public class QuizActivity extends AppCompatActivity {
         super.onResume();
     }
 
-
     @Override
     protected void onPause(){
         Log.d(SAS, "On pause");
@@ -52,123 +298,4 @@ public class QuizActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState){
-        outState.putInt(MARKER, counter);
-        outState.putInt(LOCKER_MARKER, locker ? 0 : 1);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    // Сделать несколько вопросов
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);   // Всегда самой первой
-        if(savedInstanceState != null && savedInstanceState.containsKey(MARKER))
-        {
-            counter = (int)savedInstanceState.get(MARKER) - 1;
-            locker = (int)savedInstanceState.get(LOCKER_MARKER) == 0;
-        }
-        setContentView(R.layout.activity_quiz);  // id xml внутри системы   заполняет экран макетом
-        trueButton = findViewById(R.id.true_button);
-        falseButton = findViewById(R.id.false_button);
-        quitButton = findViewById(R.id.quit_button);
-        cheatButton = findViewById(R.id.cheat_button_invoke);
-        changeText();
-
-        cheatButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivityForResult(CheatActivity.newIntent(QuizActivity.this, findAnswer()), REQUEST_CHEAT);
-                Toast.makeText(QuizActivity.this, R.string.false_ans, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        trueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(!locker)
-                {
-                    locker = true;
-                    int sas = findAnswer() ? R.string.true_ans : R.string.false_ans;
-                    Toast.makeText(QuizActivity.this, sas, Toast.LENGTH_SHORT).show();
-                    new CountDownTimer(2000, 1000) {
-                    public void onTick(long millisUntilFinished) {
-                    }
-                    public void onFinish() {
-                        changeText();
-                        locker = false;
-                    }
-                }.start();
-                }
-            }
-        });
-
-        falseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(!locker)
-                {
-                    locker = true;
-                    int sas = findAnswer() ? R.string.false_ans : R.string.true_ans;
-                    Toast.makeText(QuizActivity.this, sas, Toast.LENGTH_SHORT).show();
-                    new CountDownTimer(2000, 1000) {
-                        public void onTick(long millisUntilFinished) {
-                        }
-                        public void onFinish() {
-                            changeText();
-                            locker = false;
-                        }
-                    }.start();
-                }
-            }
-        });
-
-        quitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               finish();
-            }
-        });
-
-
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        if (requestCode == REQUEST_CHEAT) {
-            // Make sure the request was successful
-            if (resultCode == RESULT_OK) {
-                boolean sas = data.getBooleanExtra("Lol", false);
-                Toast.makeText(QuizActivity.this, R.string.cheated, Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void changeText(){
-        TextView tv = findViewById(R.id.question_view);
-        String naming = "question_" + ++counter;
-        int id = this.getResources().getIdentifier(naming, "string", getPackageName());
-        if(id == 0)
-        {
-            tv.setText("Вопросы кончились.");
-            trueButton.setEnabled(false);
-            trueButton.setVisibility(View.INVISIBLE);
-            falseButton.setEnabled(false);
-            falseButton.setVisibility(View.INVISIBLE);
-            quitButton.setEnabled(true);
-            quitButton.setVisibility(View.VISIBLE);
-        }
-        else
-            tv.setText(id);
-    }
-
-    private boolean findAnswer(){
-        String naming = "question_" + counter + "_ans";
-        int id = this.getResources().getIdentifier(naming, "string", getPackageName());
-        if(id != 0){
-            String sas = this.getResources().getString(id);
-            return sas.equals("Да");
-        }
-        return false;
-    }
 }
